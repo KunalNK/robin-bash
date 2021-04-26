cb4ec13851d37727c71ea8423dbdd1112c48bd30
kunalk07 <kunalkillekar07@gmail.com>
2021-04-26 18:15:07 +0000

    initial commit

---
+++ robin.sh
@@ -0,0 +1,1 @@
+./robin-install-k8s_5.3.5-101.sh server --username=robin --password=Robin123 --robin-image-archive=./robinbinimg-5.3.5-101.tar.gz --k8s-image-archive=./k8s-images-5.3.5-101.tar.gz --ignore-warnings
